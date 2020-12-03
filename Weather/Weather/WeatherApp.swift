a733902169f815ed95c5934f8e95a7959b547b6a
Alena Nesterkina <elena.walst@bk.ru>
2020-12-03 12:49:14 +0300

    - create project

---
+++ Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Alena Nesterkina on 3.12.20.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
